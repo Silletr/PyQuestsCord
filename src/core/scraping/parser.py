0ac756f791140ed34388d4b28ad562f0eaf5fa3e
Silletr <hristicenkoigor764@gmail.com>
2026-01-03 19:54:03 +0200

    [NEW FILE/DIR: src/core/database/*, src/core/scraping/parser.py] Added DB file, added basic database interaction (Read-Instert), also added basic parsing structure, soon i'll add data structure to take it apart and parse already only RPC ID + Game Name

--- src/core/scraping/parser.py
+++ src/core/scraping/parser.py
@@ -1,1 +1,39 @@
 # LINK FOR SCRAPING: https://discord.com/api/v10/applications/detectable
+import sqlite3 as sq
+import requests as req
+
+# from bs4 import BeautifulSoup   <- Black deleting this import as unused
+from loguru import logger
+
+# Request to link
+response = req.get("https://discord.com/api/v10/applications/detectable")
+
+if response.status_code != 200:
+    logger.critical(f"Discord sent error! Status code is: \n{response.status_code}")
+    response.raise_for_status()
+
+
+data = response.json()
+print("Data type:", type(data))
+# logger.success(response.json())
+
+
+db = sq.connect("../database/games_info.db")
+cursor = db.cursor()
+
+cursor.execute(
+    """
+    INSERT OR IGNORE INTO games VALUES (?, ?, ?)
+    """,
+    ("test", "123", 15),
+)
+db.commit()
+
+cursor.execute(
+    """
+SELECT * FROM games
+"""
+)
+print(cursor.fetchall())
+
+db.close()
