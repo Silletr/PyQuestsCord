8f77212fb2407c15136ea802811246134e08edde
Silletr <hristicenkoigor764@gmail.com>
2026-01-04 12:35:07 +0200

    [CHANGED FILE/DIR: src/core/scraping/parser.py, src/core/database/game_info.db] Added parsing of data, adding new data from API to Database - in future i'll use game name and ID with PyPresence

--- src/core/scraping/parser.py
+++ src/core/scraping/parser.py
@@ -6,7 +6,10 @@ import requests as req
 from loguru import logger
 
 # Request to link
-response = req.get("https://discord.com/api/v10/applications/detectable")
+paramaters = {"key1": "value1", "key2": "value2"}
+response = req.get(
+    "https://discord.com/api/v10/applications/detectable", params=paramaters
+)
 
 if response.status_code != 200:
     logger.critical(f"Discord sent error! Status code is: \n{response.status_code}")
@@ -15,9 +18,17 @@ if response.status_code != 200:
 
 data = response.json()
 print("Data type:", type(data))
-# logger.success(response.json())
+print(len(data))
+first_app = data[0]  # First detectable application
+game_id = first_app["id"]
+game_name = first_app.get("name") or first_app["executables"][0]["name"].replace(
+    ".exe", ""
+)
+print(f"ID: {game_id}")
+print(f"Game: {game_name}")
 
 
+# Database interaction
 db = sq.connect("../database/games_info.db")
 cursor = db.cursor()
 
@@ -25,7 +36,7 @@ cursor.execute(
     """
     INSERT OR IGNORE INTO games VALUES (?, ?, ?)
     """,
-    ("test", "123", 15),
+    (game_name, game_id, 10),
 )
 db.commit()
 
