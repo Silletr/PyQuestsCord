21499ef68f81c40cbe6e58ce517d8b8728c0aaa5
Silletr <hristicenkoigor764@gmail.com>
2026-03-02 11:53:48 +0200

    [CHANGED FILE/DIR: src/core/database/games_info.db, src/core/scraping/parser.py] Finally it working. Well, its added not whole list, but also 20k+ is added (22019 was added). Nice start, but not enough as for me xD [TIMESTAMP: 02.03.2026 | 11:53]

--- src/core/scraping/parser.py
+++ src/core/scraping/parser.py
@@ -1,15 +1,10 @@
 # LINK FOR SCRAPING: https://discord.com/api/v10/applications/detectable
 import sqlite3 as sq
 import requests as req
-
-# from bs4 import BeautifulSoup   <- Black deleting this import as unused
 from loguru import logger
 
 # Request to link
-paramaters = {"key1": "value1", "key2": "value2"}
-response = req.get(
-    "https://discord.com/api/v10/applications/detectable", params=paramaters
-)
+response = req.get("https://discord.com/api/v10/applications/detectable")
 
 if response.status_code != 200:
     logger.critical(f"Discord sent error! Status code is: \n{response.status_code}")
@@ -18,33 +13,26 @@ if response.status_code != 200:
 
 data = response.json()
 print("Data type:", type(data))
-print(len(data))
-first_app = data[0]  # First detectable application
-game_id = first_app["id"]
-game_name = first_app.get("name") or first_app["executables"][0]["name"].replace(
-    ".exe", ""
-)
-print(f"ID: {game_id}")
-print(f"Game: {game_name}")
-
+print(data.keys() if isinstance(data, dict) else data[:2])
+print(f"Len of whole games list: {len(data)}")
 
 # Database interaction
 db = sq.connect("../database/games_info.db")
 cursor = db.cursor()
 
-cursor.execute(
-    """
-    INSERT OR IGNORE INTO games VALUES (?, ?, ?)
-    """,
-    (game_name, game_id, 10),
-)
+cursor.execute("DELETE FROM games")
 db.commit()
 
-cursor.execute(
-    """
-SELECT * FROM games
-"""
-)
-print(cursor.fetchall())
+for app in data:
+    game_id = app["id"]
+    game_name = app.get("name") or app["executables"][0]["name"].replace(".exe", "")
+    cursor.execute("INSERT OR IGNORE INTO games VALUES (?, ?)", (game_name, game_id))
+
+
+db.commit()
+print(f"Inserted {len(data)} games")
+
+cursor.execute("SELECT COUNT(*) FROM games")
+print(f"Total in DB: {cursor.fetchone()[0]}")
 
 db.close()
