1af7b4bb567b21d21673bfca3e87f85ba81beaf7
Silletr <hristicenkoigor764@gmail.com>
2026-03-05 13:41:48 +0200

    [CHANGED FILE/DIR: src/core/scraping/find_game.py | NEW FILE/DIR: .github/] Added Pull Request Template, also added dynamic search the need game (yes, looks like I was not too focused yesterday that wrote about it yesterday, but not today) - now im sure in this :D [TIMESTAMP: 13:41 | 05/03/2026]

--- src/core/scraping/find_game.py
+++ src/core/scraping/find_game.py
@@ -4,7 +4,7 @@ import sqlite3 as sq
 conn = sq.connect(DB_PATH)
 cursor = conn.cursor()
 
-game_name = "Grand Theft "
+game_name = input("Enter the game name (corresponding correct name): ")
 cursor.execute("SELECT * FROM games WHERE game_name LIKE ?", (f"%{game_name}%",))
 
 rows = cursor.fetchall()
